aac3fae944b99e30b32b7d182fb2d12531fdb013
Sijal Zahra <syedasijalzahra@gmail.com>
2023-09-06 21:38:24 +0500

    commit

--- script.js
+++ script.js
@@ -28,40 +28,111 @@ function renderComments() {
         <div class="name-com">
           <div class="SA">SA</div>
           <div class="name-time">
-            <div class="commentor-name">Syed Abbass</div>
+            <div class="commentor-name">Syed Abbas</div>
             <div class="date-time">${getCurrentDateTime()}</div>
           </div>
         </div>
         <div>
-          <button class="delete-comment-btn" 
+          <button class="delete-comment-btn"
             onclick="
-              postComments.splice(${i}, 1);
+              const index = postComments.indexOf('${post}');
+              postComments.splice(index, 1);
               saveCommentsToLocalStorage();
-              renderComments();
-            "
-          >
+              renderComments();">
             <i class="fa-solid fa-trash" style="color: #5c93ff;"></i>
           </button>
         </div>
       </div>
     </div>
     `;
+
+
+
+    postCommentsHtml += html;    
+  }
+
+  document.querySelector('.posted-comments').innerHTML = postCommentsHtml;
+  const addCommentsMsg = document.querySelector('.add-comments');
+    addCommentsMsg.style.display = postComments.length === 0 ? 'block' : 'none';
+}
+
+function addComment() {
+    const inputComment = document.querySelector('.post-comment');
+    const comment = inputComment.value;
+  
+    if (postComments.includes(comment)) {
+        const duplicateMsg = document.querySelector('.duplicate-comment-msg');
+        duplicateMsg.style.display = 'block';
+        setTimeout(() => {
+          duplicateMsg.style.display = 'none';
+        }, 2000);  
+    } else if   (postComments.length >= 10) {
+        const limitMsg = document.querySelector('.comment-limit-msg');
+        limitMsg.style.display = 'block';
+        setTimeout(() => {
+          limitMsg.style.display = 'none';
+        }, 2000);
+    } else if (comment) {
+      postComments.push(comment);
+      saveCommentsToLocalStorage();
+  
+      inputComment.value = '';
+      renderComments();
+  
+      const successMsg = document.querySelector('.comment-success-msg');
+      successMsg.style.display = 'block';
+      setTimeout(() => {
+        successMsg.style.display = 'none';
+      }, 2000);
+    }
+  }
+
+const searchInput = document.querySelector('.search-input');
+searchInput.addEventListener('input', filterComments);
+
+function filterComments() {
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredComments = postComments.filter((comment) =>
+    comment.toLowerCase().includes(searchTerm)
+  );
+  renderFilteredComments(filteredComments);
+}
+
+function renderFilteredComments(filteredComments) {
+  let postCommentsHtml = '';
+
+  for (let i = 0; i < filteredComments.length; i++) {
+    const post = filteredComments[i];
+    const html = `
+      <div class="complete-comment">
+        <div>${post}</div>
+        <div class="comments-info">
+          <div class="name-com">
+            <div class="SA">SA</div>
+            <div class="name-time">
+              <div class="commentor-name">Syed Abbas</div>
+              <div class="date-time">${getCurrentDateTime()}</div>
+            </div>
+          </div>
+          <div>
+            <button class="delete-comment-btn"
+              onclick="
+                const index = postComments.indexOf('${post}');
+                postComments.splice(index, 1);
+                saveCommentsToLocalStorage();
+                renderFilteredComments(postComments);
+              "
+            >
+              <i class="fa-solid fa-trash" style="color: #5c93ff;"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    `;
     postCommentsHtml += html;
   }
 
   document.querySelector('.posted-comments').innerHTML = postCommentsHtml;
-}
-
-function addComment() {
-  const inputComment = document.querySelector('.post-comment');
-  const comment = inputComment.value;
-
-  postComments.push(comment);
-  saveCommentsToLocalStorage();
-
-  inputComment.value = '';
-
-  renderComments();
 }
 
 function saveCommentsToLocalStorage() {
